472d81f620146becaa189a7c16cae3e208929b24
asdfghjklmum <chenqi63656@gmail.com>
2021-10-12 18:54:06 +0800

    Add a timer to the game play scene
    If timer reaches 20 seconds, user will lose the game and If collects all the coins within 20 seconds, user will win the game

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -8,7 +9,13 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     int score;
+    int totalscore = 60;
+    float timeleft;
+    float starttime = 20f;
+    int timeremaining;
+
     public Text scoretxt;
+    public Text timertxt;
 
     void Awake()
     {
@@ -21,6 +28,23 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         score = 0;
+        timeleft = starttime;
+    }
+
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        timeremaining = Mathf.FloorToInt(timeleft % 60);
+        timertxt.text = "Timer: " + timeremaining.ToString();
+
+        if(score == totalscore)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if(timeleft <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     public void addscore(int add)
