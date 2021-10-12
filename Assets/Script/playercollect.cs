c2341881a8ddbbfca813a223bc5d8b53ff0a71a2
asdfghjklmum <chenqi63656@gmail.com>
2021-10-12 19:39:48 +0800

    coin effect, player walk, jump, landing sound

--- Assets/Script/playercollect.cs
+++ Assets/Script/playercollect.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class playercollect : MonoBehaviour
 {
+    public GameObject explodeEff;
+
     void OnTriggerEnter(Collider collider)
     {
         if(collider.gameObject.tag == "Player")
         {
+            Instantiate(explodeEff, transform.position, transform.rotation);
             GameManager.instance.addscore(10);
             Destroy(gameObject);
         }
